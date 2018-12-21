a1a42d3d13b4f436808403e3b1aa85834df796a7
farahwalid <farahw103@gmail.com>
2018-12-21 12:25:48 +0200

    classAuth,post,postcont,loststuff

--- implementation/src/LostStuff.java
+++ implementation/src/LostStuff.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class LostStuff {
+    private String category;
+    private String color;
+    private String size;
+    public LostStuff(String category, String color, String size){
+        this.size=size;
+        this.category=category;
+        this.color=color;
+    }
+    public void setCategory(String category){
+        this.category=category;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setSize(String size){
+        this.size=size;
+    }
+    public String getCategory(){
+        return category;
+    }
+
+    public String getSize(){
+        return size;
+    }
+
+    public String getColor(){
+        return color;
+    }
 }
--- implementation/src/postControl.java
+++ implementation/src/postControl.java
@@ -1,2 +1,17 @@
 public class postControl {
+
+    public Boolean checkPhotoExistance(String photoPath){
+        return true;
+    }
+    public void AskForConfirmation(){
+
+    }
+    public void askForPhoto(){
+
+    }
+    public void SendSubmissionFailedMsg(){
+        System.out.println("SubmissionFailed");
+    }
+    public Post CreatePost(){
+    }
 }
