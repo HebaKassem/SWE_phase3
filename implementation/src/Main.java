39d314232d011bf3202bd01c1f2da038f6f97e04
Hagar Farouk <hagar.faroukk@outlook.com>
2018-12-22 00:04:13 +0200

    LoginFunction()

--- implementation/src/Main.java
+++ implementation/src/Main.java
@@ -2,22 +2,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Database db = new Database();
+        db.readDB();
+        User u = new User();
+        boolean flag = true;
+        int choice;
+        do {
+            System.out.println("1) createAccount   2)Login  3)terminate Program");
+            choice = sc.nextInt();
 
-        Database db=new Database();
-//        db.readDB();
+            if (choice == 1)
+                u.createAccount(db);
+
+            else if (choice == 2) {
+                u.Login(db);
+
+            } else if (choice == 3)
+                flag = false;
+
+            while (choice != 3) {
+                System.out.println("1) PostPhoto   2)searchPost   3)LogOut");
+                choice = sc.nextInt();
+
+                if (choice == 1)
+                    u.PostPhoto(db);
+
+                else if (choice == 2) {
+                    u.searchPost(db);
+                }
+            }
 
 
-        Scanner in=new Scanner(System.in);
-        System.out.println("how many accounts to create ?");
-        User u=new User();
-
-        int numOfAcc=in.nextInt();
-        for(int i=0;i<numOfAcc;i++)
-            u.createAccount();
-
+        } while (flag);
         db.saveDB();
 
 
     }
-
-}
