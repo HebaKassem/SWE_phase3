9295c6d9b12d9d7e8f0d26d8ebe2135d23d1b390
farahwalid <farahw103@gmail.com>
2018-12-21 12:54:02 +0200

    classAuthcontr,searchcont

--- implementation/src/AuthenticationControl.java
+++ implementation/src/AuthenticationControl.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class AuthenticationControl {
+
+    public void sendOwnerShipQuestions(){
+
+    }
+
+    public void checkAnswersCorrection(){
+
+    }
+
+    public void showContactNumber(){
+
+    }
+
+    public void sendIncorrectAnswers(){
+        System.out.println("Incorrect Answers");
+    }
+
+    public ArrayList<String> getAnswers(){
+
+
+    }
+
+    public void removePost(Post p){
+
+    }
 }
--- implementation/src/Database.java
+++ implementation/src/Database.java
@@ -6,5 +6,6 @@ public class Database {
     public static ArrayList<Account> accounts=new ArrayList<Account>();
     public static ArrayList<Post> posts=new ArrayList<Post>();
     public static ArrayList<Authentication> authentications=new ArrayList<Authentication>();
+    public static ArrayList<String> categories = new ArrayList<>();
 
 }
